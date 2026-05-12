872b383eda035ab6aacce2f6af3d68998e3f0fe1
Justin <justwells111@gmail.com>
2026-05-12 14:16:26 -0400

    various improvements

--- hangman.py
+++ hangman.py
@@ -1,201 +1,172 @@
 import random
 from os import system
-import sys
 
-#opens the file and reads each of the lines in it, removing any whitespace, then choosing a random index in the listOfWords list.
-#if the length of the chosen word is less than 3, choose a new word.
-#if the length of word is at least 3, return that word.
-def getword():
-    #need to change the location of the words list used here.
-    wordFile = open("C:/Users/Desktop/wordList.txt","r")
-    tempwords = wordFile.readlines()
-    listofwords = []
-    for i in range(len(tempwords)):
-        listofwords.append(tempwords[i].strip())
-    word = listofwords[random.randint(0,len(listofwords)-1)]
-    while(len(word) < 3):
-        word = listofwords[random.randint(0,len(listofwords)-1)]
-    return word
+HANGMAN_STAGES = [
+r"""
+ _________
+|        |
+|        |
+|        
+|
+|
+|
+|
+|__________
+""",
+r"""
+ _________
+|        |
+|        |
+|        O
+|
+|
+|
+|
+|__________
+""",
+r"""
+ _________
+|        |
+|        |
+|        O
+|        |
+|        |
+|
+|
+|__________
+""",
+r"""
+ _________
+|        |
+|        |
+|        O
+|       \|
+|        |
+|
+|
+|___________
+""",
+r"""
+ _________
+|        |
+|        |
+|        O
+|       \|/
+|        |
+|
+|
+|___________
+""",
+r"""
+ _________
+|        |
+|        |
+|        O
+|       \|/
+|        |
+|       /
+|
+|___________
+""",
+r"""
+ _________
+|        |
+|        |
+|        O
+|       \|/
+|        |
+|       / \
+|
+|___________
+"""
+]
 
-#if there is a space in the word, add two spaces to the output for formatting. makes it more clear that there are separate words.
-def set_guessing_word(word):
-    newWord = ""
-    for i in range(len(word)):
-        if(word[i]==" "):
-            newWord += "  "
-        else:
-            newWord+= "_ "
-    return newWord
+# opens the file and reads each of the lines in it, removing any whitespace, then choosing a random word in the words list.
+def get_random_word():
+    # need to change the location of the words list used here.
+    with open("C:/Users/justw/Desktop/wordList.txt", "r") as f:
+        words = [word.strip() for word in f.readlines() if word.strip().isalpha() and len(word.strip()) >= 5]
+    return random.choice(words).lower()
 
-#set the guessed letter at each occurence.
-def update_guessing_word(pos,guessing_word,letter):
+
+# replace the letters with '_ '
+def set_guessing_word(word: str):
+    return " ".join("_" for _ in word)
+
+
+# set the guessed letter at each occurence.
+def update_guessing_word(pos, guessing_word, letter):
     word = list(guessing_word)
     for i in pos:
-        #this is necessary because of the spaces in guessing_word.
-        word[i+i] = letter
+        # this is necessary because of the spaces in guessing_word.
+        word[i + i] = letter
     return word
 
-#to win, it's based on the value of correct_count. since spaces can't and shouldn't be guessed, for each space, increment x. return it at the end to add it to correct_count.
-def get_spaces_to_update_correct_count(word):
-    x = 0
-    for i in word:
-        if(i==" "):
-            x+=1
-    return x
 
-#this one should be pretty easy to figure out. just prints out the hangman character based on how many guesses the player has left.
-def print_hangman(errors_remaining):
-    if(errors_remaining==6):
-        print(" ________________")
-        print("|		 |")
-        print("|		 |")
-        print("|	         |")
-        print("|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|_____________________")
-    elif(errors_remaining==5):
-        print(" ________________")
-        print("|		 |")
-        print("|		 |")
-        print("|	         |")
-        print("|	  	 _")
-        print("|	       /   \\")
-        print("|	       \ _ /")
-        print("|\n|\n|\n|\n|\n|\n|\n|\n|\n|_____________________")
-    elif(errors_remaining==4):
-        print(" ________________")
-        print("|		 |")
-        print("|		 |")
-        print("|	         |")
-        print("|	  	 _")
-        print("|	       /   \\")
-        print("|	       \ _ /")
-        print("|		 |")
-        print("|		 |")
-        print("|		 |")
-        print("|		 |")
-        print("|		 |")
-        print("|\n|\n|\n|_____________________")
-    elif(errors_remaining==3):
-        print(" ________________")
-        print("|		 |")
-        print("|		 |")
-        print("|	         |")
-        print("|	  	 _")
-        print("|	       /   \\")
-        print("|	       \ _ /")
-        print("|		 |")
-        print("|	       \ | ")
-        print("|		\| ")
-        print("|		 |")
-        print("|		 |")
-        print("|\n|\n|\n|_____________________")
-    elif(errors_remaining==2):
-        print(" ________________")
-        print("|		 |")
-        print("|		 |")
-        print("|	         |")
-        print("|	  	 _")
-        print("|	       /   \\")
-        print("|	       \ _ /")
-        print("|		 |")
-        print("|	       \ | /")
-        print("|		\|/ ")
-        print("|		 |")
-        print("|		 |")
-        print("|\n|\n|\n|_____________________")
-    elif(errors_remaining==1):
-        print(" ________________")
-        print("|		 |")
-        print("|		 |")
-        print("|	         |")
-        print("|	  	 _")
-        print("|	       /   \\")
-        print("|	       \ _ /")
-        print("|		 |")
-        print("|	       \ | /")
-        print("|		\|/ ")
-        print("|		 |")
-        print("|		 |")
-        print("|		/")
-        print("|	       /")
-        print("|\n|_____________________")
-    elif(errors_remaining==0):
-        print(" ________________")
-        print("|		 |")
-        print("|		 |")
-        print("|	         |")
-        print("|	  	 _")
-        print("|	       /x x\\")
-        print("|	       \ _ /")
-        print("|		 |")
-        print("|	       \ | /")
-        print("|		\|/ ")
-        print("|		 |")
-        print("|		 |")
-        print("|		/ \\")
-        print("|	       /   \\")
-        print("|\n|_____________________")
-    
-#print out the incorrectly guessed letters.    
-def print_wrong_letters(letters):
-    print("Wrong letters: ", end=" ")
-    for x in letters:
-        print(x, end=" ")
+# if the letter is in the word, return the position(s) that the letter occurs in the word.
+def get_positions(letter: str, word: str):
+    return [pos for pos in range(len(word)) if word[pos] == letter]
 
-#if the letter is in the word, return the position(s) that the letter occurs in the word.
-def get_positions(letter, word):
-    pos = []
-    for x in range(len(word)):
-        if(word[x]==letter):
-            pos.append(x)
-    return pos
 
-choice = 0
+# initializes everything for the game, and runs a loop while the user hasn't won/lost. 
+# it updates the UI to reflect how the user is doing, and shows messages based on how the user performs.
+def game_loop():
+    word = get_random_word()
+    wrong_letters = []
+    correct_count: int = 0
+    errors_remaining: int = 6
+    guessing_word = set_guessing_word(word)
 
-#this is just the main loop. there isn't anything crazy going on here, so I don't see a reason to comment it much.
-#it essentially initializes everything for the game, and runs a loop while the user hasn't won/lost. it updates the UI to reflect how the user is doing, and shows messages based on if the user ends up winning or losing.
-while(choice!=2):
-    print("\nEnter a choice:\n1)Play\n2)Quit")
-    choice = int(input())
-    if(choice!=2):
-        random.seed()   
-        word = getword()
-        wrong_letters = []
-        correct_count = 0
-        errors_remaining = 6
-        guessing_word = set_guessing_word(word)
-        correct_count += get_spaces_to_update_correct_count(word)
-        while(True):
-            system('cls')
-            print_hangman(errors_remaining)
-            print("\n" + guessing_word)
-            print_wrong_letters(wrong_letters)
-            print("\nChances left:", end=" ")
-            print(errors_remaining)
-            print("Enter a letter:", end=" ")
-            letter = input()
-            while(letter in wrong_letters or letter in guessing_word):
-                print("You already tried that letter! Try again!")
-                letter = input()
-            if(letter in word):
-                x = get_positions(letter, word)
-                guessing_word = ''.join(update_guessing_word(x, guessing_word, letter))
-                print(guessing_word)
-                correct_count += len(x)
-                if(correct_count == len(word)):
-                    system('cls')
-                    print("YOU WIN!")
-                    print_hangman(errors_remaining)
-                    print("\n" + guessing_word)
-                    print_wrong_letters(wrong_letters)
-                    print("\nChances left: ", end=" ")
-                    print(errors_remaining)
-                    break
+    while errors_remaining > 0:
+        system('cls')
+        print(HANGMAN_STAGES[6 - errors_remaining])
+        print("\n" + guessing_word)
+        print(f"Wrong letters: {' '.join(wrong_letters)}")
+        while True:
+            try:
+                letter = input("Enter a letter: ").strip().lower()
+                if not letter.isalpha() or letter in wrong_letters or letter in guessing_word or len(letter) > 1:
+                    raise ValueError()
+                break
+            except ValueError:
+                print("You must enter a valid and unused letter!")               
+        if letter in word:
+            positions = get_positions(letter, word)
+            guessing_word = ''.join(update_guessing_word(
+                positions, guessing_word, letter))
+            print(guessing_word)
+            correct_count += len(positions)
+            if correct_count == len(word):
+                system('cls')
+                print("YOU WON!")
+                print(HANGMAN_STAGES[6 - errors_remaining])
+                print("\n" + guessing_word)
+                break
+        else:
+            wrong_letters.append(letter)
+            errors_remaining -= 1
+            if (errors_remaining == 0):
+                system('cls')
+                print('YOU LOST!')
+                print(HANGMAN_STAGES[6 - errors_remaining])
+                print(f"The word was {word}")
+                break
+
+
+# calls the game loop to play, and handles main input
+def main():
+    while True:
+        try:
+            choice = int(input("Enter a choice:\n1) Play\n2) Quit\nInput: "))
+            if choice not in [1, 2]:
+                raise ValueError()
+            if choice == 1:
+                game_loop()
             else:
-                wrong_letters.append(letter)
-                errors_remaining-=1
-                if(errors_remaining==0):
-                    system('cls')
-                    print_hangman(errors_remaining)
-                    print("You lose!\nThe word was:", end = " ")
-                    print(word)
-                    break
+                exit()
+        except ValueError:
+            print("You must enter either 1 or 2!")
+
+
+if __name__ == "__main__":
+    main()
